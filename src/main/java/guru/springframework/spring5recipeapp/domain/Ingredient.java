4112635e6b5e81c7e34f72fbfe3008ceb884154f
Alejandra Blanco <aleblancoa.3@gmail.com>
2019-06-24 17:44:05 -0500

    View ingredients

--- src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -1,8 +1,13 @@
 package guru.springframework.spring5recipeapp.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
 
@@ -12,49 +17,21 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    @ManyToOne
-    private Recipe recipe;
+    public Ingredient(){
+
+    }
 
     @OneToOne(fetch = FetchType.EAGER)
     private  UnitOfMeasure uom;
 
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
+    @ManyToOne
+    private Recipe recipe;
 
-    public void setUom(UnitOfMeasure uom) {
+    public Ingredient(String description, BigDecimal amount,  UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.recipe = recipe;
         this.uom = uom;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 }
